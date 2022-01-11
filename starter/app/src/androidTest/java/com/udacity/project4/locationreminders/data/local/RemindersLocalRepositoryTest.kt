295b81e1ae6a786f3275559f0d507583e9cf0688
Angad Singh <angad.singh@olx.com>
2022-01-12 01:24:41 +0530

    Added tests for RemindersLocalRepositoryTest.kt

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -10,13 +10,7 @@ import com.udacity.project4.locationreminders.data.dto.Result
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.instanceOf
-import org.hamcrest.MatcherAssert.assertThat
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
@@ -25,6 +19,76 @@ import org.junit.runner.RunWith
 @MediumTest
 class RemindersLocalRepositoryTest {
 
-//    TODO: Add testing implementation to the RemindersLocalRepository.kt
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var remindersRepository: RemindersLocalRepository
+    private lateinit var database: RemindersDatabase
+
+    private val testData = ReminderDTO(
+        "Title",
+        "Description",
+        "location",
+        0.0, 0.0
+    )
+
+    @Before
+    fun createDatabase() {
+        database = Room.databaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java, "locationReminders.db"
+        )
+        .allowMainThreadQueries()
+        .build()
+        remindersRepository = RemindersLocalRepository(database.reminderDao(), Dispatchers.Main)
+    }
+
+    @Test
+    fun testInsertReminder() = runBlocking {
+        remindersRepository.saveReminder(testData)
+
+        val result = remindersRepository.getReminders()
+        Assert.assertTrue(result is Result.Success)
+
+        result as Result.Success
+
+        Assert.assertEquals("No items are inserted in the database", 1, result.data.size)
+        Assert.assertTrue("The list doesn't contain the inserted item", result.data.contains(testData))
+    }
+
+    @Test
+    fun testReminderById() = runBlocking {
+        remindersRepository.saveReminder(testData)
+
+        val result = remindersRepository.getReminder(testData.id)
+        Assert.assertTrue(result is Result.Success)
+
+        result as Result.Success
+
+        val savedData = result.data
+        Assert.assertEquals("Titles are not equal", testData.title, savedData.title)
+        Assert.assertEquals("Descriptions are not equal", testData.description, savedData.description)
+        Assert.assertEquals("Locations are not equal", testData.location, savedData.location)
+        Assert.assertEquals("Latitudes are not equal", testData.latitude, savedData.latitude)
+        Assert.assertEquals("Longitudes are not equal", testData.longitude, savedData.longitude)
+    }
+
+    @Test
+    fun testDeletionAll() = runBlocking {
+        remindersRepository.saveReminder(testData)
+        remindersRepository.deleteAllReminders()
+
+        val result = remindersRepository.getReminders()
+        Assert.assertTrue(result is Result.Success)
+
+        result as Result.Success
+
+        Assert.assertTrue("There are no reminders in the database", result.data.isEmpty())
+    }
+
+    @After
+    fun clearDatabase() {
+        database.clearAllTables()
+        database.close()
+    }
 }
