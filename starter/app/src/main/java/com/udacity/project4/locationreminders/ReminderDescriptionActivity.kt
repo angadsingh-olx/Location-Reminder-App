63cb56a1ab5d10fdf87611bbdb53e2ee58c1ab13
Angad Singh <angad.singh@olx.com>
2022-01-12 00:13:47 +0530

    Implemented show location detail

--- starter/app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
@@ -5,14 +5,26 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityReminderDescriptionBinding
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+
+import com.google.android.gms.maps.model.MarkerOptions
+
+import com.google.android.gms.maps.model.LatLng
+
+
+
+
 /**
  * Activity that displays the reminder details after the user clicks on the notification
  */
-class ReminderDescriptionActivity : AppCompatActivity() {
+class ReminderDescriptionActivity : AppCompatActivity(), OnMapReadyCallback {
 
     companion object {
         private const val EXTRA_ReminderDataItem = "EXTRA_ReminderDataItem"
@@ -32,6 +44,22 @@ class ReminderDescriptionActivity : AppCompatActivity() {
             this,
             R.layout.activity_reminder_description
         )
-//        TODO: Add the implementation of the reminder details
+
+        val reminderData = intent.getSerializableExtra(EXTRA_ReminderDataItem) as ReminderDataItem
+        binding.reminderDataItem = reminderData
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+    override fun onMapReady(mapView: GoogleMap?) {
+        val reminderData = intent.getSerializableExtra(EXTRA_ReminderDataItem) as ReminderDataItem
+        val sydney = LatLng(reminderData.latitude?:0.0, reminderData.longitude?:0.0)
+        mapView?.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title(reminderData.title)
+        )
+        mapView?.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f))
     }
 }
