9c05da806046fcafc293acb8bd33f62a247dc0da
Angad Singh <angad.singh@olx.com>
2022-01-12 12:14:44 +0530

    Cleanup

--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -131,7 +131,3 @@ class SaveReminderFragment : BaseFragment() {
         _viewModel.onClear()
     }
 }
-
-private const val REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE = 33
-private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
-private const val REQUEST_TURN_DEVICE_LOCATION_ON = 29
